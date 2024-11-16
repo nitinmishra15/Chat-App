11e319c355619a31a2cc533ab3406e7673fbf95a
nitinmishra15 <nitinmishra1500@gmail.com>
2024-11-16 15:21:40 +0530

    app completed

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser"; 
 import userRoute from "./routes/user.route.js";
 import messageRoute  from "./routes/message.route.js";
@@ -30,6 +31,15 @@ try{
 }
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+if(process.env.NODE_ENV === "production"){
+  const dirPath = path.resolve();
+
+  app.use(express.static("./frontend/dist"));
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(dirPath, "./frontend/dist","index.html"));
+  })
+}
 
 
 server.listen(PORT, () => {
